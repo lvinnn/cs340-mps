1c30c5055a41ff0bdfe9c16127bab819bdd1dadb
Wade Fagen-Ulmschneider <waf@illinois.edu>
2023-01-24 15:49:03 -0600

    mp1 release

---
+++ mp1/emoji.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+// Return your favorite emoji.  Do not allocate new memory.
+// (This should **really** be your favorite emoji, we plan to use this later in the semester. :))
+char *emoji_favorite() {
+  return "";
+}
+
+
+// Count the number of emoji in the UTF-8 string `utf8str`, returning the count.  You should
+// consider everything in the ranges starting from (and including) U+1F000 up to (and including) U+1FAFF.
+int emoji_count(const unsigned char *utf8str) {
+  return 0;
+}
+
+
+// Return a random emoji stored in new heap memory you have allocated.  Make sure what
+// you return is a valid C-string that contains only one random emoji.
+char *emoji_random_alloc() {
+  return NULL;
+}
+
+
+// Modify the UTF-8 string `utf8str` to invert the FIRST character (which may be up to 4 bytes)
+// in the string if it the first character is an emoji.  At a minimum:
+// - Invert "😊" U+1F60A ("\xF0\x9F\x98\x8A") into ANY non-smiling face.
+// - Choose at least five more emoji to invert.
+void emoji_invertChar(unsigned char *utf8str) {
+
+}
+
+
+// Modify the UTF-8 string `utf8str` to invert ALL of the character by calling your
+// `emoji_invertChar` function on each character.
+void emoji_invertAll(unsigned char *utf8str) {
+
+}
+
+
+// Reads the full contents of the file `fileName, inverts all emojis, and
+// returns a newly allocated string with the inverted file's content.
+unsigned char *emoji_invertFile_alloc(const char *fileName) {
+  return NULL;
+}
