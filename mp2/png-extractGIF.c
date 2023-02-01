4020882be31e5115d2c3ebc6a3837a70eb6f1af6
Wade Fagen-Ulmschneider <waf@illinois.edu>
2023-01-31 19:13:00 -0600

    mp2 release

---
+++ mp2/png-extractGIF.c
@@ -0,0 +1,5 @@
+#include "png-extractGIF.h"
+
+int png_extractGIF(const char *png_filename, const char *gif_filename) {
+  return 255;  // Change the to a zero to indicate success, when your implementaiton is complete.
+}
