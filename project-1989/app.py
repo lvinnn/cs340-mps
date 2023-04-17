3decbf845cc1458137d2211e13481237b3a5f8a4
elvin <elvinwc2@illinois.edu>
2023-04-17 17:45:59 -0500

    MP submission

--- project-1989/app.py
+++ project-1989/app.py
@@ -4,19 +4,25 @@ from PIL import Image
 import numpy as np
 from io import BytesIO
 import os
+from sklearn.neighbors import KDTree
 
 app = Flask(__name__)
 
 folder_dirs = ["static/MG1", "static/MG2"]
-av_colors = {"static/MG1" : {}, "static/MG2" : {}}
+tiles_across = 10
+av_colors = {}
+tree = {}
+for path in folder_dirs:
+  av_colors[path] = []
+  tree[path] = []
 
 for path in folder_dirs:
   for img_name in os.listdir(path):
     with Image.open(path+"/"+img_name) as img:
       img_array = np.array(img)
-      average_color = img_array.mean(axis=(0, 1))
-      average_color = average_color.astype(int)
-      av_colors[path][str(img_name)] = average_color
+      average_color = img_array.mean(axis=(0, 1)).astype(int)
+      av_colors[path].append((img_name, average_color))
+  tree[path] = KDTree([color[:3] for _, color in av_colors[path]])
 
 @app.route('/', methods=["GET"])
 def GET_index():
@@ -39,7 +45,7 @@ def POST_makeMosaic():
   return jsonify(response)
 
 def mosaic(img, mg_path):
-  tiles_across = 70
+  global tiles_across
   width, height = img.size
   tile_size = int(width/tiles_across)
 
@@ -48,17 +54,11 @@ def mosaic(img, mg_path):
       x_coord, y_coord = j*tile_size, i*tile_size
       crop = img.crop((x_coord, y_coord, x_coord + tile_size, y_coord + tile_size))
       crop_array = np.array(crop)
-      average_color = crop_array.mean(axis=(0, 1))
-      average_color = average_color.astype(int)
-      global av_colors
-      min_dist = 999999999999
-      min_img = None
-      for col in av_colors[mg_path]:
-        distance = np.linalg.norm(av_colors[mg_path][col][0:3] - average_color[0:3], axis=0)
-        if distance < min_dist:
-          min_dist = distance
-          min_img = col
-      
+      average_color = crop_array.mean(axis=(0, 1)).astype(int)[0:3]
+      global tree
+      distances, ind = tree[mg_path].query(average_color.reshape(1, -1))
+      min_img = av_colors[mg_path][ind[0][0]][0]
+
       with Image.open(mg_path+"/"+min_img) as sample_image:
         min_dim = min(sample_image.width, sample_image.height)
         img_cropped = sample_image.crop((0, 0, min_dim, min_dim))
@@ -67,7 +67,7 @@ def mosaic(img, mg_path):
 
       img.paste(crop, (x_coord,y_coord))
     
-  img.crop((0,0,tiles_across*tile_size,int(height/tile_size)*tile_size))
+  img = img.crop((0,0,tiles_across*tile_size,int(height/tile_size)*tile_size))
 
   buffer = BytesIO()
   img.save(buffer, format='PNG')
